17fbd4f3541b0538f376aa3fc3b77728790fceed
kotauchisunsun <kotauchisunshine@hotmail.co.jp>
2022-09-17 13:39:04 +0900

    fix

--- docs/main.js
+++ docs/main.js
@@ -3,16 +3,31 @@
 // 1eth = 1,000,000,000,000,000,000 wei
 // 1,000円 = 0.0045 eth
 
+function convertEthToYen(eth) { 
+    const bid = 203446;
+    const ask = 208597;
+    const avg = (bid + ask) / 2;
+    
+    return eth * avg;    
+}
+
+function writeElementText(id, value) { 
+    document.getElementById(id).innerText = `${id}: ${value}`;
+}
+
 const params = new URLSearchParams(document.location.search.substring(1));
+const shopName = params.get("shopName");
 const to = params.get("to");
 const eth = params.get("eth");
 const wei = Web3.utils.toWei(eth, 'ether');
-console.log(to);
-console.log(eth);
 
-document.getElementById("to").innerText = "to: " + to;
-document.getElementById("eth").innerText = "eth: " + eth;
-document.getElementById("yen").innerText = "yen: " + parseFloat(eth) * 220000;
+const button = document.getElementById("pay");
+button.disabled = true;
+
+writeElementText("shopName", shopName);
+writeElementText("to",  to);
+writeElementText("sendEth", eth);
+writeElementText("sendYen", convertEthToYen(parseFloat(eth)));
 
 const provider = window.ethereum;
 const web3 = new Web3(provider);
@@ -20,12 +35,45 @@ const web3 = new Web3(provider);
 web3.eth.requestAccounts().then(result => { 
     const address = result[0];
     console.log(address);
-    web3.eth.sendTransaction(
-        {
-            "from": address,
-            "to": to,
-            "value": wei,
-            "chain": "rinkeby"
+    writeElementText("account", address);
+
+    web3.eth.net.getId().then(
+        (id) => { 
+            if (id != 4) {
+                alert("Select Account Rikeby Network!!");
+                return;
+            }
+      
+            web3.eth.getBalance(address)
+            .then(
+                (balanceWei) => {
+                    const balanceEth = Web3.utils.fromWei(balanceWei);
+                    writeElementText("balanceEth", balanceEth);
+                    const balanceYen = convertEthToYen(balanceEth);
+                    writeElementText("balanceYen", balanceYen);
+                }
+            );
+
+            button.disabled = false;
+            button.onclick = () => {
+                button.disabled = true;
+                web3.eth.sendTransaction(
+                    {
+                        "from": address,
+                        "to": to,
+                        "value": wei
+                    }
+                ).then(
+                    (result) => {
+                        const { transactionHash, gasUsed } = result;
+                        writeElementText("transactionHash", transactionHash);
+                        writeElementText("gasUsed", gasUsed);
+                        setTimeout(() => {
+                            alert("Sended!!");
+                        },100);
+                    }
+                );
+            };
         }
     );
 });
